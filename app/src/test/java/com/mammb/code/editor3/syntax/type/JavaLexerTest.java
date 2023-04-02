27b97cbc3fdb30cbf2bc7c66a13ed82cb59db7f9
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2023-04-02 14:05:58 +0900

    Fix test

--- app/src/test/java/com/mammb/code/editor3/syntax/type/JavaLexerTest.java
+++ app/src/test/java/com/mammb/code/editor3/syntax/type/JavaLexerTest.java
@@ -1,6 +1,7 @@
 package com.mammb.code.editor3.syntax.type;
 
 import com.mammb.code.editor3.syntax.LexerSource;
+import com.mammb.code.editor3.syntax.ScopeType;
 import com.mammb.code.editor3.syntax.Token;
 import com.mammb.code.editor3.syntax.TokenType;
 import org.junit.jupiter.api.Test;
@@ -13,12 +14,12 @@ class JavaLexerTest {
     void nextToken() {
 
         var lexer = JavaLexer.of(LexerSource.of("public static void main(String... args) {"));
-        assertEquals(new Token(TokenType.KEYWORD.ordinal(), 0, 6), lexer.nextToken());
-        assertEquals(new Token(TokenType.SP.ordinal(), 6, 1), lexer.nextToken());
-        assertEquals(new Token(TokenType.KEYWORD.ordinal(), 7, 6), lexer.nextToken());
-        assertEquals(new Token(TokenType.SP.ordinal(), 13, 1), lexer.nextToken());
-        assertEquals(new Token(TokenType.KEYWORD.ordinal(), 14, 4), lexer.nextToken());
-        assertEquals(new Token(TokenType.SP.ordinal(), 18, 1), lexer.nextToken());
-        assertEquals(new Token(TokenType.ANY.ordinal(), 19, 4), lexer.nextToken());
+        assertEquals(new Token(TokenType.KEYWORD.ordinal(), ScopeType.NEUTRAL, 0, 6), lexer.nextToken());
+        assertEquals(new Token(TokenType.SP.ordinal(), ScopeType.NEUTRAL, 6, 1), lexer.nextToken());
+        assertEquals(new Token(TokenType.KEYWORD.ordinal(), ScopeType.NEUTRAL, 7, 6), lexer.nextToken());
+        assertEquals(new Token(TokenType.SP.ordinal(), ScopeType.NEUTRAL, 13, 1), lexer.nextToken());
+        assertEquals(new Token(TokenType.KEYWORD.ordinal(), ScopeType.NEUTRAL, 14, 4), lexer.nextToken());
+        assertEquals(new Token(TokenType.SP.ordinal(), ScopeType.NEUTRAL, 18, 1), lexer.nextToken());
+        assertEquals(new Token(TokenType.ANY.ordinal(), ScopeType.NEUTRAL, 19, 4), lexer.nextToken());
     }
 }
