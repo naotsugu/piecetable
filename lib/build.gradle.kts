9a08abeb1c014b325512e588a68ed261319d5cbf
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2023-05-02 17:47:36 +0900

    Add gradle publish

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -37,3 +37,46 @@ tasks.jar {
     }
 }
 
+
+publishing {
+    publications {
+        register<MavenPublication>("gpr") {
+            artifactId = "piecetable"
+            from(components["java"])
+            pom {
+                name.set("piecetable")
+                description.set("Java implementation of PieceTable data structure.")
+                url.set("https://github.com/naotsugu/piecetable")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("naotsugu")
+                        name.set("Naotsugu Kobayashi")
+                        email.set("naotsugukobayashi@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("git@github.com:naotsugu/piecetable.git")
+                    developerConnection.set("git@github.com:naotsugu/piecetable.git")
+                    url.set("https://github.com/naotsugu/piecetable")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/naotsugu/piecetable")
+            credentials {
+                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+            }
+        }
+    }
+}
+
