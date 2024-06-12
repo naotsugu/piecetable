dc4c85cf9eb148d82c19182d20048c3268a3087d
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-06-12 18:17:12 +0900

    Bump up foojay-resolver-convention 0.7 -> 0.8

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,5 +2,5 @@ rootProject.name = "piecetable"
 include("lib", "app")
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version("0.7.0")
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
 }
