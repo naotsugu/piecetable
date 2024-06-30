d06e2e7dfba36d4bb363475abf2e0f6ceec294b1
Naotsugu Kobayashi <naotsugukobayashi@gmail.com>
2024-06-30 18:31:38 +0900

    Modify LineIndex

--- dev/src/main/java/com/mammb/dev/picetable/index/LineIndex.java
+++ dev/src/main/java/com/mammb/dev/picetable/index/LineIndex.java
@@ -1,18 +1,47 @@
+/*
+ * Copyright 2022-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mammb.dev.picetable.index;
 
 import java.util.Arrays;
 
+/**
+ * The LineIndex.
+ * Holds the byte length of each line as an index.
+ * @author Naotsugu Kobayashi
+ */
 public class LineIndex {
 
+    /** The line lengths. */
     private int[] lineLengths;
+    /** The length of line lengths array. */
     private int length;
 
-    /** sub-total cache. */
+    /** The sub-total cache. */
     private long[] stCache;
+    /** The sub-total cache length. */
     private int cacheLength;
+    /** The sub-total cache interval. */
     private final int cacheInterval;
 
-    public LineIndex(int cacheInterval) {
+
+    /**
+     * Create a new {@code LineIndex}.
+     * @param cacheInterval the sub-total cache interval
+     */
+    private LineIndex(int cacheInterval) {
         lineLengths = new int[0];
         length = 0;
 
@@ -21,16 +50,31 @@ public class LineIndex {
         this.cacheInterval = cacheInterval;
     }
 
+
+    /**
+     * Create a new {@code LineIndex}.
+     * @return a new {@code LineIndex}
+     */
     public static LineIndex of() {
         return new LineIndex(100);
     }
 
+
+    /**
+     * Create a new {@code LineIndex}.
+     * @param cacheInterval the sub-total cache interval
+     * @return a new {@code LineIndex}
+     */
     static LineIndex of(int cacheInterval) {
         return new LineIndex(cacheInterval);
     }
 
 
-    void add(byte[] bytes) {
+    /**
+     * Adds the specified byte array to the index.
+     * @param bytes the specified byte array to be added
+     */
+    public void add(byte[] bytes) {
 
         int[] lines = lines(bytes);
         if (lines.length == 0) {
@@ -58,7 +102,12 @@ public class LineIndex {
     }
 
 
-    long get(int lineNum) {
+    /**
+     * Gets the byte length of the specified line.
+     * @param lineNum the specified line
+     * @return the byte length of the specified line
+     */
+    public long get(int lineNum) {
 
         int startLine = 0;
         long startPos = 0;
@@ -83,7 +132,13 @@ public class LineIndex {
         return startPos;
     }
 
-    void insert(int lineNum, int posAtLine, byte[] bytes) {
+    /**
+     * Insert the specified byte array to the index.
+     * @param lineNum the specified line
+     * @param posAtLine the specified position in a line
+     * @param bytes the specified byte array to be inserted
+     */
+    public void insert(int lineNum, int posAtLine, byte[] bytes) {
         int[] lines = lines(bytes);
         if (lines.length == 0) {
             return;
@@ -95,8 +150,10 @@ public class LineIndex {
         cacheLength = lineNum / cacheInterval;
 
         if (lines.length == 1) {
+            // insert operation within a single line
             lineLengths[lineNum] += lines[0];
         } else {
+            // insert operation across multiple lines
             System.arraycopy(lineLengths, lineNum + 1,
                 lineLengths, lineNum + lines.length,
                 length - (lineNum + 1));
@@ -110,7 +167,15 @@ public class LineIndex {
         length += lines.length - 1;
     }
 
-    void delete(int lineNum, int posAtLine, int len) {
+
+    /**
+     * Delete the specified byte length of the index.
+     * @param lineNum the specified line
+     * @param posAtLine the specified position in a line
+     * @param len the specified byte length to be deleted
+     */
+    public void delete(int lineNum, int posAtLine, int len) {
+
         if (len <= 0) {
             return;
         }
@@ -118,23 +183,27 @@ public class LineIndex {
         cacheLength = lineNum / cacheInterval;
 
         if ((lineLengths[lineNum] - posAtLine) > len) {
-            // Delete operation within a single line
+
+            // delete operation within a single line
             // |a|b|c|d|$|       ->        |a|d|$|
             //   ^---  posAtLine:1, len:2
             lineLengths[lineNum] -= len;
+
         } else {
-            // Delete operation across multiple lines
-            // 0 |a|b|$|    delete(             |a|f|$|
-            // 1 |c|d|$|      lineNum = 0,      |g|h|$|
+
+            // delete operation across multiple lines
+            // 0 |a|b|$|    delete(          ->   |a|f|$|
+            // 1 |c|d|$|      lineNum = 0,        |g|h|$|
             // 2 |e|f|$|      posAtLine = 1,
             // 3 |g|h|$|      len = 6)
+
             len -= lineLengths[lineNum] - posAtLine;
             lineLengths[lineNum] = posAtLine;
             int lines = 0;
             do {
                 len -= lineLengths[lineNum + ++lines];
             } while (len > 0);
-            lineLengths[lineNum] += (-len); // Merge the rest to the first line
+            lineLengths[lineNum] += (-len); // merge the rest to the first line
             System.arraycopy(
                 lineLengths, lineNum + 1 + lines,
                 lineLengths, lineNum + 1,
@@ -143,6 +212,12 @@ public class LineIndex {
         }
     }
 
+
+    /**
+     * Converts the specified byte array to line-by-line byte length.
+     * @param bytes the specified byte array
+     * @return line-by-line byte length
+     */
     static int[] lines(byte[] bytes) {
 
         if (bytes == null || bytes.length == 0) {
@@ -164,6 +239,12 @@ public class LineIndex {
         return intArray.get();
     }
 
+
+    /**
+     * Grow this lineLengths array.
+     * @param minCapacity the growth capacity
+     * @return the grown int array
+     */
     private int[] grow(int minCapacity) {
         int oldCapacity = lineLengths.length;
         if (oldCapacity > 0) {
@@ -176,6 +257,12 @@ public class LineIndex {
         }
     }
 
+
+    /**
+     * Grow this sub-total cache array.
+     * @param minCapacity the growth capacity
+     * @return the grown long array
+     */
     private long[] growCache(int minCapacity) {
         int oldCapacity = stCache.length;
         if (oldCapacity > 0) {
@@ -188,10 +275,18 @@ public class LineIndex {
         }
     }
 
+    /**
+     * Gets the line lengths array.
+     * @return the line lengths array
+     */
     int[] lineLengths() {
         return Arrays.copyOf(lineLengths, length);
     }
 
+    /**
+     * Gets the sub-total cache array.
+     * @return the sub-total cache array
+     */
     long[] stCache() {
         return Arrays.copyOf(stCache, cacheLength);
     }
